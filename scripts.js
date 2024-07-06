272804d0055df0049384410d2605fe44a6712941
droid-kk11 <153240992+droid-kk11@users.noreply.github.com>
2024-07-05 17:30:59 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,54 @@
+function generateQRCode(){
+    let location = document.getElementById("location").value;
+    let first_name = document.getElementById("first").value;
+    let last_name = document.getElementById("last").value;
+    let team_number = document.getElementById("teamno").value;
+    let match_number = document.getElementById("match").value;
+    let swerve = document.getElementById("swerve").checked ? "yes" : "no";
+    let defense = document.getElementById("defense").checked ? "yes" : "no";
+    let offense = document.getElementById("offense").checked ? "yes" : "no";
+    let ss1 = document.getElementById("ss1").checked ? "yes" : "no";
+    let ss1_label = document.getElementById("ss1-label").innerHTML;
+    let ss2 = document.getElementById("ss2").checked ? "yes" : "no";
+    let ss2_label = document.getElementById("ss2-label").innerHTML;
+    let ss3 = document.getElementById("ss3").checked ? "yes" : "no";
+    let ss3_label = document.getElementById("ss3-label").innerHTML;
+    let ss4 = document.getElementById("ss4").checked ? "yes" : "no";
+    let ss4_label = document.getElementById("ss4-label").innerHTML;
+    let ss5 = document.getElementById("ss5").checked ? "yes" : "no";
+    let ss5_label = document.getElementById("ss5-label").innerHTML;
+
+    function addLeadingZero(num) {
+        return num < 10 ? '0' + num : num;
+    }
+    
+    let current_date = new Date();
+    let datetime = current_date.getFullYear() + "-"
+                    + addLeadingZero(current_date.getMonth() + 1) + "-"
+                    + addLeadingZero(current_date.getDate()) + "@"
+                    + addLeadingZero(current_date.getHours()) + ":"  
+                    + addLeadingZero(current_date.getMinutes()) + ":" 
+                    + addLeadingZero(current_date.getSeconds());
+
+    let output = `SCOUTING DATA:
+Date: ${datetime}
+Event location: ${location}
+Scouter's name: ${first_name} ${last_name}
+Team: ${team_number}, match ${match_number}
+Swerve: ${swerve}
+Defense: ${defense}
+Offense: ${offense}
+${ss1_label} ${ss1}
+${ss2_label} ${ss2}
+${ss3_label} ${ss3}
+${ss4_label} ${ss4}
+${ss5_label} ${ss5}
+USING SCOUTING APP BY TEAM 2898`;
+
+    output = output.replace(/\?/g, ":");
+
+    //alert(output);
+    /*const blob = new Blob([output], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);*/
+    new QRCode(document.getElementById("qrcode"), output);
+}
