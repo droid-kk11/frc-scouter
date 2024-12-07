9b70713f1d50eb4982a36747995db12fc1d573fd
droid-kk11 <153240992+droid-kk11@users.noreply.github.com>
2024-12-07 13:28:17 -0800

    Local storage update

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 var deferredPrompt;
+var scoutData;
+var numberOfScouts;
+var current_scout;
 
 window.addEventListener("beforeinstallprompt", (e) => {
     e.preventDefault();
@@ -13,34 +16,34 @@ window.addEventListener("appinstalled", () => {
     console.log("App installed!");
 });
 
-function generateFile() {
-    var location = document.getElementById("location").value;
-    var first_name = document.getElementById("first").value;
-    var last_name = document.getElementById("last").value;
-    var team_number = document.getElementById("teamno").value;
-    var match_number = document.getElementById("match").value;
-    var swerve = document.getElementById("swerve").value;
-    var defense = document.getElementById("defense").value;
-    var offense = document.getElementById("offense").value;
-    var score = document.getElementById("score").value;
-    var drive = document.getElementById("drive").value;
-    var speed = document.getElementById("speed").value;
-    var ss1 = document.getElementById("ss1").checked ? "yes" : "no";
-    var ss1_label = "Speaker?";
-    var ss2 = document.getElementById("ss2").checked ? "yes" : "no";
-    var ss2_label = "Amp?";
-    var ss3 = document.getElementById("ss3").checked ? "yes" : "no";
-    var ss3_label = "Harmony?";
-    var ss4 = document.getElementById("ss4").checked ? "yes" : "no";
-    var ss4_label = "Climb?";
-    var ss5 = document.getElementById("ss5").checked ? "yes" : "no";
-    var ss5_label = "Trap?";
-    var died = document.getElementById("died").checked ? "yes" : "no";
-    var dropped = document.getElementById("dropped").checked ? "yes" : "no";
+function addLeadingZero(num) {
+    return num < 10 ? '0' + num : num;
+}
 
-    function addLeadingZero(num) {
-        return num < 10 ? '0' + num : num;
-    }
+function generateFile() {
+    let location = document.getElementById("location").value;
+    let first_name = document.getElementById("first").value;
+    let last_name = document.getElementById("last").value;
+    let team_number = document.getElementById("teamno").value;
+    let match_number = document.getElementById("match").value;
+    let swerve = document.getElementById("swerve").value;
+    let defense = document.getElementById("defense").value;
+    let offense = document.getElementById("offense").value;
+    let score = document.getElementById("score").value;
+    let drive = document.getElementById("drive").value;
+    let speed = document.getElementById("speed").value;
+    let ss1 = document.getElementById("ss1").checked ? "yes" : "no";
+    let ss1_label = "Speaker?";
+    let ss2 = document.getElementById("ss2").checked ? "yes" : "no";
+    let ss2_label = "Amp?";
+    let ss3 = document.getElementById("ss3").checked ? "yes" : "no";
+    let ss3_label = "Harmony?";
+    let ss4 = document.getElementById("ss4").checked ? "yes" : "no";
+    let ss4_label = "Climb?";
+    let ss5 = document.getElementById("ss5").checked ? "yes" : "no";
+    let ss5_label = "Trap?";
+    let died = document.getElementById("died").checked ? "yes" : "no";
+    let dropped = document.getElementById("dropped").checked ? "yes" : "no";
 
     let current_date = new Date();
     let datetime = current_date.getFullYear() + "-"
@@ -82,17 +85,17 @@ END_DATA`;
 }
 
 function checkIfUnanswered() {
-    var location = document.getElementById("location").value;
-    var first_name = document.getElementById("first").value;
-    var last_name = document.getElementById("last").value;
-    var team_number = document.getElementById("teamno").value;
-    var match_number = document.getElementById("match").value;
-    var swerve = document.getElementById("swerve").value;
-    var defense = document.getElementById("defense").value;
-    var offense = document.getElementById("offense").value;
-    var score = document.getElementById("score").value;
-    var drive = document.getElementById("drive").value;
-    var speed = document.getElementById("speed").value;
+    let location = document.getElementById("location").value;
+    let first_name = document.getElementById("first").value;
+    let last_name = document.getElementById("last").value;
+    let team_number = document.getElementById("teamno").value;
+    let match_number = document.getElementById("match").value;
+    let swerve = document.getElementById("swerve").value;
+    let defense = document.getElementById("defense").value;
+    let offense = document.getElementById("offense").value;
+    let score = document.getElementById("score").value;
+    let drive = document.getElementById("drive").value;
+    let speed = document.getElementById("speed").value;
 
     let required = [
         {name: "Event location", value: location},
@@ -133,4 +136,87 @@ Add to local storage: Adds the current scout data to local storage, so a file do
 Create file: Uses the current scout data and data in local storage (if applicable) and generates a CSV file. Local storage will be cleared.
     `;
     alert(message);
+}
+
+function checkIfLocalStorageInitialized(){
+    if (!localStorage.getItem("scoutnumber") === null && !localStorage.getItem("scoutdata") === null){
+        console.log("Scouter data exists!");
+    }
+
+    else {
+        console.log("Creating scouter data...");
+        let scoutNumber = ["scouts", 0];
+        let scoutingData = {
+            "data": []
+        }
+        console.log(scoutNumber);
+        console.log(scoutingData);
+        localStorage.setItem("scoutnumber", JSON.stringify(scoutNumber));
+        localStorage.setItem("scoutdata", JSON.stringify(scoutingData));
+    }
+}
+
+function createScoutingData(){
+    let location = document.getElementById("location").value;
+    let first_name = document.getElementById("first").value;
+    let last_name = document.getElementById("last").value;
+    let team_number = document.getElementById("teamno").value;
+    let match_number = document.getElementById("match").value;
+    let swerve = document.getElementById("swerve").value;
+    let defense = document.getElementById("defense").value;
+    let offense = document.getElementById("offense").value;
+    let score = document.getElementById("score").value;
+    let drive = document.getElementById("drive").value;
+    let speed = document.getElementById("speed").value;
+    let ss1 = document.getElementById("ss1").checked ? "yes" : "no";
+    let ss1_label = "Speaker?";
+    let ss2 = document.getElementById("ss2").checked ? "yes" : "no";
+    let ss2_label = "Amp?";
+    let ss3 = document.getElementById("ss3").checked ? "yes" : "no";
+    let ss3_label = "Harmony?";
+    let ss4 = document.getElementById("ss4").checked ? "yes" : "no";
+    let ss4_label = "Climb?";
+    let ss5 = document.getElementById("ss5").checked ? "yes" : "no";
+    let ss5_label = "Trap?";
+    let died = document.getElementById("died").checked ? "yes" : "no";
+    let dropped = document.getElementById("dropped").checked ? "yes" : "no";
+
+    let data_dict = {
+        "location": location,"first_name": first_name,
+        "last_name": last_name,"team_number": team_number,
+        "match_number": match_number,"swerve": swerve,
+        "defense": defense, "offense": offense,
+        "score": score, "drive": drive,
+        ss1_label: ss1, ss2_label: ss2, ss3_label: ss3,
+        ss4_label: ss4, ss5_label: ss5, "died": died,
+        "dropped": dropped
+    }
+    if (numberOfScouts == 0){
+        scoutData = [];
+        scoutData.push(data_dict);
+    }
+
+    else {
+        scoutData.push(data_dict);
+    }
+    alert(data_dict);
+
+    current_scout = data_dict;
+}
+
+function exportScoutingData(){
+    let scoutNumber = ["scouts", numberOfScouts];
+    let scoutingData = scoutData;
+    alert(scoutNumber);
+    alert(scoutingData);
+    localStorage.setItem("scoutnumber", JSON.stringify(scoutNumber));
+    localStorage.setItem("scoutdata", JSON.stringify(scoutingData));
+    
+}
+
+function importScoutingData(){
+    numberOfScouts = JSON.parse(localStorage.getItem("scoutnumber"));
+    numberOfScouts = numberOfScouts[1];
+    scoutData = JSON.parse(localStorage.getItem("scoutdata"));
+    document.getElementById("lsavailability").innerHTML = `(${numberOfScouts} scout(s) available)`;
 }
